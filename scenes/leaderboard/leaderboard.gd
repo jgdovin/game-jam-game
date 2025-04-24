6e1ec522bf195c6917eacf8f7b95e60927c80d1b
Josh Gdovin <joshgdovin@gmail.com>
2025-04-23 19:02:13 -0600

    fix leaderboard issue

--- scenes/leaderboard/leaderboard.gd
+++ scenes/leaderboard/leaderboard.gd
@@ -31,7 +31,7 @@ func _ready() -> void:
 func _show_player_score() -> void:
 	var score_instance = score_line.instantiate()
 	score_instance.data = {"name": Game.player_emojis, "score": Game.current_score, "timestamp": Time.get_datetime_string_from_system(false, true), "metadata": {"difficulty": Game.difficulty, "words_completed": Game.words_completed, "words_lost": Game.words_in_fire, "typos_made": Game.typos_made, "typo_modes_triggered": Game.typo_modes_triggered}, "is_current_player": true}
-	player_score_container.add_child(score_instance)
+	player_score.add_child(score_instance)
 
 func _on_home_pressed() -> void:
 	References.game_controller.load_main_menu()
